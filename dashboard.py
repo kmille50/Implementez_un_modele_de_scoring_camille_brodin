f0d200630a88e467316238fa0b80471264b51beb
Camille Brodin <camillebrodin@outlook.fr>
2023-12-19 18:23:53 +0100

    Avancement 19122023

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import api
 import requests
+import json
+import numpy as np
+import plotly.figure_factory as ff
 
 url = "http://127.0.0.1:5000/predict"
 
@@ -11,56 +14,61 @@ dataset = st.container()
 features = st.container()
 model_training = st.container()
 
-output = st.text("")
-
 with header:
        st.title("Prêt à dépenser : Solvabilité client")
        st.text("Dashboard interactif pour plus de transparence sur les décisions d'octroi de crédit, et mise à disposition des clients leurs informations personnelles avec exploration facilité.")
 
 with dataset:
-       st.header("Donnees informations clients")
+       st.header("Déja client ? Consultez vos données en toute transparence!")
        st.text("Ce dataset est disponible a cette adresse: https://www.kaggle.com/competitions/home-credit-default-risk/data")
-       clients_data = pd.read_csv('X_train.csv')
-       st.write(clients_data.head(3))
+       clients_data = pd.read_csv('X_tr.csv')     
        
-       st.subheader("Distribution du type des contrats de crédit")
-       type_credit = pd.DataFrame(clients_data['NAME_CONTRACT_TYPE'].value_counts()).head()
-       st.bar_chart(type_credit)
+       id = st.selectbox("Identifiant client:", clients_data['SK_ID_CURR'].unique())
+       st.write(clients_data.loc[clients_data['SK_ID_CURR'] == id])
        
        st.subheader("Distribution des annuités de prêt")
-       annuite = pd.DataFrame(clients_data['AMT_ANNUITY'].value_counts()).head()
+       annuite = pd.DataFrame(clients_data['CREDIT_INCOME_PERCENT'].value_counts()).head()
        st.bar_chart(annuite)
        
+       st.subheader("Distribution du type des contrats de crédit")
+       type_credit = pd.DataFrame(clients_data['TARGET'].value_counts()).head()
+       st.bar_chart(type_credit)
+       
+       
 with model_training:
+
+       st.header('Vous réfléchissez à prendre un crédit ? Faites une simulation dès maintenant!')
+       st.text("Ici vous pouvez tester des informations clients pour générer une prédiction de solvabilité:")        
        
-       def request_prediction(model_uri, data):
-              headers = {"Content-Type": "application/json"}
-
-              data_json = {'data': data}
-              response = requests.request(
-              method='POST', headers=headers, url=model_uri, json=data_json)
-
-              if response.status_code != 200:
-                     raise Exception("Request failed with status {}, {}".format(response.status_code, response.text))
-
-              return response.json()
-
-       st.header('Entrainons le modèle de prédiction!')
-       st.text("Ici vous pouvez tester des informations clients pour générer une prédiction de solvabilité:")
-                     
-       sel_col, disp_col = st.columns(2)
-                     
-       annuite_val = st.number_input("Quel montant d'annuité de prêt souhaiteriez vous?", min_value = 2000, max_value = 225000, value=24930, step=500)
-                     
-              #type_credit_val = sel_col.selectbox("Quel type de contrat souhaiteriez vous?", clients_data['NAME_CONTRACT_TYPE'].unique())
-              # annuite_val = sel_col.slider("Quel montant d'annuité de prêt souhaiteriez vous?", min_value = 2000, max_value = 225000, value=24930, step=500)
-              # #input_feature = sel_col.text_input("Quelle variable d'entrée ?", "")
-                     
-       predict_btn = st.button('Prédire')
-       if predict_btn:
-              data = [[annuite_val]]
-
-              pred = request_prediction(url, data)[0]
-              st.write('Le prix médian d\'une habitation est de {:.2f}'.format(pred))
-
+       DAYS_BIRTH = st.number_input('DAYS_BIRTH', min_value=21., max_value=70., value=44., step=1.)
+       DAYS_EMPLOYED = st.number_input('DAYS_EMPLOYED', min_value=0., max_value=18000., value=2395., step=1.)
+       CREDIT_TERM = st.number_input('CREDIT_TERM', min_value=0., max_value = 1., value=0.05, step=0.1)
+       CREDIT_INCOME_PERCENT = st.number_input('CREDIT_INCOME_PERCENT', min_value=0., max_value =40., value=4., step=1.)
+       EXT_SOURCE_1 = st.number_input('EXT_SOURCE_1', min_value=0., max_value =1., value=0.5, step=0.1)
+       EXT_SOURCE_2 = st.number_input('EXT_SOURCE_2', min_value=0., max_value =1., value=0.5, step=0.1)
+       EXT_SOURCE_3 = st.number_input('EXT_SOURCE_3', min_value =0., max_value =1., value=0.5, step=0.1)
+       ORGANIZATION_TYPE = st.selectbox('ORGANIZATION_TYPE', clients_data['ORGANIZATION_TYPE'].unique())
+       OCCUPATION_TYPE = st.selectbox(' OCCUPATION_TYPE', clients_data['OCCUPATION_TYPE'].unique())
+       CNT_CHILDREN = st.number_input('CNT_CHILDREN', min_value =0., max_value =10., value=0., step=1.)
        
+
+       if 'prediction' not in st.session_state:
+              st.session_state.prediction = {}
+
+       def request_model():
+              params = {
+                    "DAYS_BIRTH": DAYS_BIRTH, "DAYS_EMPLOYED": DAYS_EMPLOYED, "CREDIT_TERM" : CREDIT_TERM,
+                    "CREDIT_INCOME_PERCENT" : CREDIT_INCOME_PERCENT, "EXT_SOURCE_1" : EXT_SOURCE_1, "EXT_SOURCE_2" : EXT_SOURCE_2,
+                    "EXT_SOURCE_3" : EXT_SOURCE_3, "ORGANIZATION_TYPE" : ORGANIZATION_TYPE, "OCCUPATION_TYPE" : OCCUPATION_TYPE,
+                    "CNT_CHILDREN" : CNT_CHILDREN
+                    }
+              
+              response = requests.post(url, json=params)
+              response.status_code == 200
+              st.session_state.prediction = response.json()
+              
+       
+       st.button('Click me', on_click=request_model)
+
+       if st.session_state.prediction:
+              st.write(st.session_state.prediction)
